62ddae14a1187c9a0d090fa356f1a960ea045748
Andrey Moura <andreymds88@gmail.com>
2025-01-19 11:43:46 -0300

    internal: Retrieve executable path (Windows)

--- src/file.cpp
+++ src/file.cpp
@@ -1,5 +1,10 @@
 #include <uva/file.hpp>
 
+#ifdef _WIN32
+#   include <Windows.h>
+#   undef min
+#endif
+
 void uva::file::write_all_bytes(const std::filesystem::path& path, const uint8_t* bytes, size_t len)
 {
     write_all_bytes(path.string(), bytes, len);
@@ -28,6 +33,10 @@ std::filesystem::path uva::file::executable_path()
 {
 #ifdef __linux__
     return std::filesystem::read_symlink("/proc/self/exe");
+#elif _WIN32
+    char buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    return std::filesystem::path(buffer);
 #else
     throw std::runtime_error("unsupported platform");
 #endif
